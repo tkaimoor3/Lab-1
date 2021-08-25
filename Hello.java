b3677e90411f8f2356398f912271f432f92571de
tkaimoor3 <89538367+tkaimoor3@users.noreply.github.com>
2021-08-25 12:49:15 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	public static void main(String[] args){
+	System.out.println("<Tkai Moore>");
+	System.out.println("<Hello.java>");
+	System.out.println("<Lab 1>");
+	System.out.println("<082521>");
+	System.out.println("<255 C90>");
+	System.out.println();
+	}
+}
